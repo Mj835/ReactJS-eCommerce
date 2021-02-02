d28c10b05faacbb272589c5030c2aa61cdfed05a
Muskan Jaiswal <muskanmahak777@gmail.com>
2021-02-03 00:24:35 +0530

    Project Completed

--- redux-store/src/components/Cart.js
+++ redux-store/src/components/Cart.js
@@ -1,22 +1,24 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { BsDash, BsPlus, BsReverseBackspaceReverse } from "react-icons/bs";
-
+import { BsDash, BsPlus } from "react-icons/bs";
+import { BsReverseBackspaceReverse } from "react-icons/bs";
 const Cart = () => {
-  const { products } = useSelector((state) => state.CartReducer);
-  const dispatch = useDispatch;
+  const { products, totalQuantities, totalPrice } = useSelector(
+    (state) => state.CartReducer
+  );
+  const dispatch = useDispatch();
   return (
     <div className="cart">
       <div className="container">
-        <h3>Your Cart</h3>
+        <h3>Your cart</h3>
         {products.length > 0 ? (
           <>
             <div className="row">
               <div className="col-9">
-                <div className="cart_heading">
+                <div className="cart__heading">
                   <div className="row">
                     <div className="col-2">Picture</div>
-                    <div className="col-2">Item Name</div>
+                    <div className="col-2">Name</div>
                     <div className="col-2">Price</div>
                     <div className="col-2">Inc/Dec</div>
                     <div className="col-2">Total Price</div>
@@ -26,21 +28,21 @@ const Cart = () => {
                 {products.map((product) => (
                   <div className="row verticalAlign" key={product.id}>
                     <div className="col-2">
-                      <div className="cart_image">
+                      <div className="cart__image">
                         <img src={`/images/${product.image}`} alt="" />
                       </div>
                     </div>
                     <div className="col-2">
-                      <div className="cart_name">{product.name}</div>
+                      <div className="cart__name">{product.name}</div>
                     </div>
                     <div className="col-2">
-                      <div className="cart_price">
+                      <div className="cart__price">
                         ₹ {product.discountPrice}
                       </div>
                     </div>
                     <div className="col-2">
-                      <div className="details_info">
-                        <div className="details_incDec">
+                      <div className="details__info cart__incDec">
+                        <div className="details__incDec">
                           <span
                             className="dec"
                             onClick={() =>
@@ -62,13 +64,13 @@ const Cart = () => {
                       </div>
                     </div>
                     <div className="col-2">
-                      <div className="cart_total text-center">
+                      <div className="cart__total text-center">
                         ₹ {product.discountPrice * product.quantity}
                       </div>
                     </div>
                     <div className="col-2">
                       <div
-                        className="cart_remove"
+                        className="cart__remove"
                         onClick={() =>
                           dispatch({ type: "REMOVE", payload: product.id })
                         }
@@ -79,7 +81,24 @@ const Cart = () => {
                   </div>
                 ))}
               </div>
-              <div className="col-3">Summary</div>
+              <div className="col-3 summary-col">
+                <div className="summary">
+                  <div className="summary__heading">Summary</div>
+                  <div className="summary__details">
+                    <div className="row mb-10">
+                      <div className="col-6">Total Items:</div>
+                      <div className="col-6">{totalQuantities}</div>
+                    </div>
+                    <div className="row mb-10">
+                      <div className="col-6">Total Price</div>
+                      <div className="col-6">₹ {totalPrice}</div>
+                    </div>
+                    <button type="button" className="checkout">
+                      Checkout
+                    </button>
+                  </div>
+                </div>
+              </div>
             </div>
           </>
         ) : (
